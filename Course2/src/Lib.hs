67cfa21692173b6f9314bd6c179a09fbe49dd587
Javier de Vega Ruiz <javier@letsgetchecked.com>
2019-09-13 23:11:09 +0100

    Build working

--- Course2/src/Lib.hs
+++ Course2/src/Lib.hs
@@ -1,17 +1,27 @@
 module Lib
     ( someFunc
-    , DnaString
+    , DnaString(..)
+    , slice
     , kmers
     ) where
 
 import qualified Data.Text as T
-import Data.String
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-data DnaString = DnaString T.Text
+data DnaString = DnaString T.Text deriving (Show, Eq)
 
-kmers :: DnaString -> [DnaStr
+dnaStringLength :: DnaString -> Int
+dnaStringLength (DnaString s) = T.length s
 
-kmers (DnaString x) = [DnaString x]
+slice :: Int -> Int -> DnaString -> DnaString
+slice startIndex count (DnaString s) =
+  DnaString $ T.take count $ T.drop startIndex s
+
+kmers :: Int -> DnaString -> [DnaString]
+kmers k dna =
+  map kmerAt [ 0 .. dnaStringLength dna - k]
+  where
+    kmerAt i = slice i k dna
+
--- Course2/test/Spec.hs
+++ Course2/test/Spec.hs
@@ -1,13 +1,23 @@
 import Test.Hspec
+import qualified Data.Text as T
 import Lib
-
 
 main :: IO ()
 main = hspec $ do
   describe "Kmers in a DnaString" $ do
     it "returns expected values for sample" $ do
-      DnaString.kmers 5 dnaString `shouldBe` kmers
+      kmers k dnaString `shouldBe` expectedKmers
       where
-        dnaString = ""
-        kmers = [""]
+        k = 5
+        dnaString = DnaString $ T.pack "ACGTACGTACGT"
+        expectedKmers = map
+                  (DnaString . T.pack)
+                  [ "ACGTA"
+                  , "CGTAC"
+                  , "GTACG"
+                  , "TACGT"
+                  , "ACGTA"
+                  , "CGTAC"
+                  , "GTACG"
+                  , "TACGT"]
 
