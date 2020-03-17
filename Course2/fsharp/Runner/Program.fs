cc795e74dc658d4530f9666e173a1faf0f021b87
Javier de Vega Ruiz <javier@letsgetchecked.com>
2020-03-17 21:19:46 +0000

    Progressgit add .

--- Course2/fsharp/Runner/Program.fs
+++ Course2/fsharp/Runner/Program.fs
@@ -20,27 +20,105 @@ type Grade = { InGrade : int; OutGrade : int }
 with
     static member balance {InGrade = a; OutGrade = b} = a - b
 
+type Edge<'a> = 'a * 'a
+
+module Edge =
+    let source (a, _) = a
+    let destination (_, b) = b
+
+type Walk<'t> = Walk of list<'t>
+
+module Walk = 
+    let empty : Walk<'t> = Walk []
+    let singleton (x : 'u) : Walk<'u> = Walk [x]
+    let append (node : 'a) (Walk xs) =
+        Walk (node :: xs)
+    let ofStartEdge ((src, dst) : Edge<'u>) : Walk<'u> =
+        singleton src |> append dst
+    let map (f : 'u -> 'v) (Walk x : Walk<'u>) : Walk<'v> = 
+        x |> List.map f |> Walk
+    // print with -> as separator
+    let print (printElem : 'a -> string) (Walk x) =
+        x
+        |> Seq.rev
+        |> Seq.map printElem
+        |> String.concat "->"
+    /// print without separator
+    let print2 (printElem : 'a -> string) (Walk x) =
+        x
+        |> Seq.rev
+        |> Seq.map printElem
+        |> System.String.Concat
+    let toArray (Walk x) =
+        x |> Seq.rev |> Seq.toArray
+    let parse (parseElem : string -> 'a) (s : string) : Walk<'a> =
+        s
+        |> splitS " -> "
+        |> Seq.map parseElem
+        |> Seq.toList
+        |> Walk
+    //let has (node : 'a) (Walk xs) =
+    //    List.contains node xs
+    let rotate (n : int) (Walk xs : Walk<'a>) : Walk<'a> =
+        let length = List.length xs
+        let nPrime = n % length
+        Seq.concat [
+            (xs |> Seq.skip nPrime)
+            (xs |> Seq.take nPrime)
+        ]
+        |> Seq.toList
+        |> Walk
+    let dropFirstIfCycle (x : Walk<'a>) : Walk<'a> =
+        match x with
+        | (Walk (h::t)) when List.last t = h -> Walk t
+        | _ -> x
+    let appendLast (Walk ws : Walk<'a> as x) : Walk<'a> =
+        let last = List.last ws
+        append last x
+    let lastNode (Walk ns : Walk<'a>) : option<'a> = 
+        match ns with
+        | [] -> None
+        | h::t -> Some h
+
 type DirectedGraph<'t> =
     | AdjacencyList of list<'t * list<'t>>
-with
-    static member map (f : 'u -> 'v) (g : DirectedGraph<'u>) : DirectedGraph<'v> =
+module DirectedGraph =
+    let startWalk ((edgeSrc, edgeDst) : Edge<'u> as edge) (AdjacencyList records) : DirectedGraph<'u> * Walk<'u> =
+        let walk = Walk.ofStartEdge edge
+        let graphMinusEdge =
+            records
+            |> List.choose (
+                fun (src, dsts) ->
+                    if src <> edgeSrc then
+                        match dsts with
+                        | [] -> None
+                        | other -> Some (src, dsts)
+                    else
+                        match dsts with
+                        | [] -> None
+                        | [singleDst] when singleDst = edgeDst -> None
+                        | other -> Some (src, other |> List.filter ((<>)edgeDst))
+            )
+            |> AdjacencyList
+        graphMinusEdge, walk
+    let map (f : 'u -> 'v) (g : DirectedGraph<'u>) : DirectedGraph<'v> =
         let (AdjacencyList records) = g
         let recordsPrime = 
             records
             |> List.map (fun (src, dsts) -> f src, dsts |> List.map f)
         AdjacencyList recordsPrime
-    static member sort (AdjacencyList records : DirectedGraph<'a> when 'a : comparison) =
+    let sort (AdjacencyList records : DirectedGraph<'a> when 'a : comparison) =
         let recordsPrime = 
             records
             |> Seq.map (fun (src, dsts) -> (src, dsts |> List.sort))
             |> Seq.sortBy fst
             |> Seq.toList
         AdjacencyList recordsPrime
-    static member print (printElem : 'a -> string) (AdjacencyList x) =
+    let print (printElem : 'a -> string) (AdjacencyList x) =
         x
         |> Seq.map (fun (a, b) -> sprintf "%s -> %s" (printElem a) (b |> Seq.map printElem |> String.concat ","))
         |> String.concat "\n"
-    static member parse (parseNode : string -> 'a) (s : string) : DirectedGraph<'a> =
+    let parse (parseNode : string -> 'a) (s : string) : DirectedGraph<'a> =
         let lines = splitLines s
         lines 
         |> Seq.filter (not << System.String.IsNullOrWhiteSpace)
@@ -52,11 +130,14 @@ with
                 | other -> failwithf "Invalid line format, fields were %A" other))
         |> Seq.toList
         |> AdjacencyList
-    static member takeFirst (AdjacencyList records) : option<'a> =
+    let takeFirst (AdjacencyList records) : option<'a> =
         match records with
         | (a, _)::_ -> Some a
         | [] -> None
-    static member walk<'a when 'a : equality> (src : 'a) (AdjacencyList records : DirectedGraph<'a>) : option<('a * 'a)> * DirectedGraph<'a> =
+    let walkFirstEdge<'a when 'a : equality> 
+        (src : 'a) 
+        (AdjacencyList records : DirectedGraph<'a>)
+        : option<Edge<'a>> * DirectedGraph<'a> =
         let zero = (None, [])
         let folder (edge, newRecords) (recordSrc, recordDsts as record) =
             match edge with
@@ -71,19 +152,19 @@ with
                 else None, record :: newRecords
         List.fold folder zero records
         |> (fun (a, b) -> a , AdjacencyList b)
-    static member isEmpty (AdjacencyList records : DirectedGraph<'a>) : bool =
+    let isEmpty (AdjacencyList records : DirectedGraph<'a>) : bool =
         List.isEmpty records
-    static member hasEdgesFor<'a when 'a : equality> (node : 'a) (AdjacencyList records : DirectedGraph<'a>) : bool =
+    let hasEdgesFor<'a when 'a : equality> (node : 'a) (AdjacencyList records : DirectedGraph<'a>) : bool =
         records
         |> Seq.exists (fst >> (=)node)
-    static member edges (AdjacencyList records : DirectedGraph<'a>) : seq<'a * 'a> =
+    let allEdges (AdjacencyList records : DirectedGraph<'a>) : seq<'a * 'a> =
         records |> Seq.collect (fun (src, dsts) -> dsts |> Seq.map (fun dst -> (src, dst)))
-    static member grades<'a when 'a : comparison> (AdjacencyList records : DirectedGraph<'a> as graph) : Map<'a, Grade> =
+    let grades<'a when 'a : comparison> (AdjacencyList records : DirectedGraph<'a> as graph) : Map<'a, Grade> =
         let grades = 
             records
             |> Seq.map (fun (src, dsts) -> src, { InGrade = 0; OutGrade = dsts.Length })
             |> Map.ofSeq
-        DirectedGraph.edges graph 
+        allEdges graph 
         |> Seq.fold
             (fun gradesPrime (_, dst) ->
                 match Map.tryFind dst gradesPrime with
@@ -91,7 +172,13 @@ with
                 | None -> Map.add dst { InGrade = 1; OutGrade = 0 } gradesPrime
             )
             grades
-    static member add (src : 'a, dst : 'a) (AdjacencyList records : DirectedGraph<'a>) : DirectedGraph<'a> =
+    let edges (vertex : 'a) (AdjacencyList records : DirectedGraph<'a>) : list<Edge<'a>> =
+        records
+        |> List.tryFind (fun (src, _) -> src = vertex)
+        |> Option.map (fun (src, dsts) -> dsts |> List.map (fun dst -> src, dst))
+        |> Option.toList
+        |> List.collect id
+    let add (src : 'a, dst : 'a) (AdjacencyList records : DirectedGraph<'a>) : DirectedGraph<'a> =
         let zero = (false, [])
         let folder ((added : bool), (recordsPrime : list<'a * list<'a>>)) (srci : 'a, dstsi : list<'a>) : bool * list<'a * list<'a>> =
             if src = srci 
@@ -99,52 +186,13 @@ with
             else added, (srci, dstsi) :: recordsPrime
         let added, recordsPrime = Seq.fold folder zero records
         AdjacencyList <| if added then recordsPrime else (src, [dst]) :: records
-            
-type Walk<'t> = Walk of list<'t>
-with
-    static member map (f : 'u -> 'v) (Walk x : Walk<'u>) : Walk<'v> = 
-        x |> List.map f |> Walk
-    // print with -> as separator
-    static member print (printElem : 'a -> string) (Walk x) =
-        x
-        |> Seq.rev
-        |> Seq.map printElem
-        |> String.concat "->"
-    /// print without separator
-    static member print2 (printElem : 'a -> string) (Walk x) =
-        x
-        |> Seq.rev
-        |> Seq.map printElem
-        |> System.String.Concat
-    static member toArray (Walk x) =
-        x |> Seq.rev |> Seq.toArray
-    static member parse (parseElem : string -> 'a) (s : string) : Walk<'a> =
-        s
-        |> splitS " -> "
-        |> Seq.map parseElem
+    let vertices (AdjacencyList records : DirectedGraph<'a>) : list<'a> =
+        records
+        |> Seq.collect (fun (src, dsts) -> src::dsts)
+        |> Seq.distinct
         |> Seq.toList
-        |> Walk
-    //static member has (node : 'a) (Walk xs) =
-    //    List.contains node xs
-    static member append (node : 'a) (Walk xs) =
-        Walk (node :: xs)
-    static member rotate (n : int) (Walk xs : Walk<'a>) : Walk<'a> =
-        let length = List.length xs
-        let nPrime = n % length
-        Seq.concat [
-            (xs |> Seq.skip nPrime)
-            (xs |> Seq.take nPrime)
-        ]
-        |> Seq.toList
-        |> Walk
-    static member dropFirstIfCycle (x : Walk<'a>) : Walk<'a> =
-        match x with
-        | (Walk (h::t)) when List.last t = h -> Walk t
-        | _ -> x
-    static member appendLast (Walk ws : Walk<'a> as x) : Walk<'a> =
-        let last = List.last ws
-        Walk.append last x
 
+  
 let deBruijnGraph<'t, 'a, 'c when 't :> seq<'a> and 'c : comparison > (comparer : seq<'a> -> 'c) (k : int) (kmers : seq<'t>) : DirectedGraph<seq<'a>> =
     let toEdge (x : seq<'a>) = (x |> Seq.take (k - 1), x |> Seq.skip 1)
     let records = 
@@ -159,7 +207,7 @@ let rec walkUntilCycle (graph : DirectedGraph<'a>) (walk : Walk<'a>) : Walk<'a> 
     match walk with
     | Walk [] -> invalidArg "walk" "Walk cannot be empty"
     | Walk (last::_) ->
-        match DirectedGraph.walk last graph with
+        match DirectedGraph.walkFirstEdge last graph with
         | Some (_, next), graphPrime -> 
             walkUntilCycle graphPrime (Walk.append next walk)
         | None, graphPrime -> walk, graphPrime
@@ -254,12 +302,33 @@ let stringSpelledByGappedPatterns (k : int) (d : int) (xs : seq<'a[]*'a[]>) : op
     else 
         None
 
-let walkNonBranchingPaths (AdjacencyList edges : DirectedGraph<'a> as graph) (vertex : 'a) :
+let walkNonBranchingPaths (grades : Map<'a, Grade>) (AdjacencyList edges : DirectedGraph<'a> as graph) (vertex : 'a) :
     DirectedGraph<'a> * list<Walk<'a>> = 
-    AdjacencyList edges, []
+    let dstEdges = DirectedGraph.edges vertex graph
+    let rec stepWithinDstEdge (graph : DirectedGraph<'a>, acc : Walk<'a>) : DirectedGraph<'a> * Walk<'a> =
+        match Walk.lastNode acc with
+        | Some src -> 
+            let lastGrade = Map.find src grades
+            if lastGrade = { InGrade = 1; OutGrade = 1}
+            then
+                let maybeEdge, graph2 = DirectedGraph.walkFirstEdge src graph
+                match maybeEdge with
+                | Some (_, dst) -> stepWithinDstEdge (graph2, Walk.append dst acc)
+                | None -> failwithf "Edge starting on %A not found" src
+            else graph, acc            
+        | None -> failwithf "Walk %A is empty" acc
+
+    let rec step (dstEdgesLeft : list<Edge<'a>>) (graph : DirectedGraph<'a>, acc : list<Walk<'a>>) =
+        match dstEdgesLeft with
+        | [] -> graph, acc
+        | edge::t -> 
+            let graph1, walk1 = DirectedGraph.startWalk edge graph
+            let graph2, walk2 = stepWithinDstEdge (graph1, walk1)
+            step t (graph2, walk2::acc)
+    step dstEdges (graph, [])
 
 let maximalNonBranchingPaths (AdjacencyList entries : DirectedGraph<'a> as graph) : Walk<'a>[] =
-    let grades = DirectedGraph<_>.grades graph
+    let grades = DirectedGraph.grades graph
     let rec step (vertices : list<'a>) (edges : DirectedGraph<'a>) (acc : list<Walk<'a>>) : list<Walk<'a>> =
         match vertices with
         | [] -> acc
@@ -267,11 +336,12 @@ let maximalNonBranchingPaths (AdjacencyList entries : DirectedGraph<'a> as graph
             let grade = Map.find h grades
             if grade <> { InGrade = 1; OutGrade = 1} && grade.OutGrade > 0
             then 
-                let edgesNext, walks = walkNonBranchingPaths edges h
+                let edgesNext, walks = walkNonBranchingPaths grades edges h
                 step t edgesNext (walks @ acc)
             else
                 step t edges acc
-    [||]
+    step (DirectedGraph.vertices graph) graph []
+    |> List.toArray
 
 let printSeq (print : 'a -> string) (xs : seq<'a>) : string =
     xs |> Seq.map print |> System.String.Concat
